8214baf1f84600268725251f52a57f9f139498bd
unknown <ribasoficial01@gmail.com>
2022-09-20 18:51:45 -0300

    Organizacao do css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import Cards from "./ProjetoSigno";
 
-
 const App = () => {
     
     return(
--- src/ProjetoSigno/index.js
+++ src/ProjetoSigno/index.js
@@ -5,53 +5,27 @@ import data from "./data"
 const Cards = () => {
     const [signo, setSigno] = useState(data)
 
-    const style= {
-        textAlign: 'center',
-        margin: '0px'
-    }
-   
-    const styleTitulo = {
-         marginTop:'20px',
-         marginBottom: '15px',
-         textAlign: 'center',
-    }
-    
-    const styleImg = {
-        width:'130px',
-         borderRadius:'4px',
-         marginTop:'8px',
-         marginBottom: '12px'
-    }
-    
-    const styleDivCards = {
-        display: 'flex',
-        flexDirection:'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-    }
-
     return(
-        <> <h3 style={styleTitulo}>Baralho de Signos</h3>
-          <div style={styleDivCards}>
-            {signo.map((sig) => {
-                
-                return(
+        <> 
+            <h3 className="titulo">Baralho de Signos</h3>
+            <div className="cards_div">
+                {signo.map((sig) => { return(
                     <div id="cards">
-                    <div key={`${signo.length}`} id="front" className="card">
-                        <h4 style={style}>{sig.nome} </h4>
-                        <img style={styleImg}src={sig.img} />
-                        <h4 style={style}>{sig.data}</h4>
+                        <div key={`${signo.length}`} id="front" className="card">
+                            <h4 className="card_front_margin">{sig.nome} </h4>
+                            <img className="card_front_img" src={sig.img} />
+                            <h4 className="card_front_margin">{sig.data}</h4>
+                        </div>
+                        <div id="back" className="card">
+                            <h4 className="card_back_titulo">{sig.nome}</h4>
+                            <p className="card_back_descricao">{sig.descricao}</p>
+                        </div>
                     </div>
-                    <div id="back" className="card">
-                        <h4 style={{padding:'0px', margin:'0px'}}>{sig.nome}</h4>
-                        <p>{sig.descricao}</p>
-                    </div>
-                </div>
-                )
-})}
+                )})}
           </div>
         </>
     )
 }
+
 
 export default Cards
